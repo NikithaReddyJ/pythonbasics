3a1da0a3041656efef817651e7af10d1f91ecff1
NikithaReddyJ <54440684+NikithaReddyJ@users.noreply.github.com>
2019-09-05 17:04:43 +0530

    Create collectiontypes.py

---
+++ collectiontypes.py
@@ -0,0 +1,34 @@
+#list
+
+l=["india","singapore","united states of america"]
+
+print(l)
+
+l.append("malaysia")
+
+print(l)
+
+del(l[2])
+
+print(l)
+
+l.insert(2,"italy")
+
+print(l)
+
+
+
+#set (no indexing)
+
+s={"india","singapore","united states of america"}
+
+print(s)
+
+s.update(["malaysia"])
+
+print(s)
+
+s.remove("singapore")
+
+print(s)
+
